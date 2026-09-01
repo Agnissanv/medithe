733b14c23b21a4a6603bcafba9b97a10d8a62d72
Agnissan Isaac Valen <valenbouge@gmail.com>
2026-09-01 02:36:55 +0000

    champ de texte enrichie complet

--- frontend/src/components/admin/RichTextField.jsx
+++ frontend/src/components/admin/RichTextField.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
 import ReactQuill from 'react-quill';
 
-const modules = { toolbar: [['bold', 'italic', 'underline', 'link']] };
+const modules = {
+  toolbar: [
+    [{ header: [2, 3, false] }],
+    ['bold', 'italic', 'underline', 'strike'],
+    [{ color: [] }, { background: [] }],
+    [{ list: 'ordered' }, { list: 'bullet' }],
+    [{ align: [] }],
+    ['blockquote'],
+    ['link'],
+    ['clean'],
+  ],
+};
+
+const formats = [
+  'header', 'bold', 'italic', 'underline', 'strike',
+  'color', 'background', 'list', 'bullet', 'align', 'blockquote', 'link',
+];
 
 export default function RichTextField({ value, onChange, placeholder }) {
-  return <ReactQuill theme="snow" value={value} onChange={onChange} modules={modules} placeholder={placeholder} />;
+  return (
+    <ReactQuill
+      theme="snow"
+      value={value}
+      onChange={onChange}
+      modules={modules}
+      formats={formats}
+      placeholder={placeholder}
+    />
+  );
 }
